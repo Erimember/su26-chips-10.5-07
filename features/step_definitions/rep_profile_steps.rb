4db536cec7c20948c678be45ab66523a49c4a3d8
Nayan Ramakrishnan <nayanr505@berkeley.edu>
2026-08-07 03:37:06 +0000

    [test] added tests for links to profile pages from search and news

---
+++ features/step_definitions/rep_profile_steps.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+Then /I should see a link to (.*)'s profile page$/ do |name|
+  rep = Representative.find_by(name: name)
+  expect(page).to have_link(name, href: representative_path(rep))
+end
+
+Given /(.*) has a news article$/ do |name|
+  rep = Representative.find_by(name: name)
+  NewsItem.create!(
+    representative: rep,
+    title: 'Test News Item',
+    link: 'https://testnews.com',
+    description: 'test'
+  )
+end
+
+When /I visit (.*)'s news articles page$/ do |name|
+  rep = Representative.find_by(name: name)
+  visit representative_news_items_path(rep)
+end
